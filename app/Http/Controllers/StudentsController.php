69c4c67fb31ad6da6fbc76eea0d609f502307fa1
criss julip <christopherkennedy459@gmail.com>
2022-01-27 06:17:12 +0300

    Second commit

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $students = Student::latest()->paginate(5);
+        // $users = DB::table('students')-paginate(5)->get();
+
+        return view('index',compact(['students']))
+            ->with(request()->input('page'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'olevel' => 'required',
+            'alevel'=>'required'
+        ]);
+
+        Student::create($request->all());
+
+        return redirect()->route('index')
+                        ->with('success','Application successful');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="pull-left">
+                <h2>Student Registration Form</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('students.create') }}"> Register Here</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Olevel</th>
+            <th>Alevel</th>
+
+        </tr>
+      @foreach ($students as $student )
+      <tr>
+        <td>{{ $student->id }}</td>
+        <td>{{ $student->name }}</td>
+        <td>{{ $student->olevel }}</td>
+        <td>{{ $student->alevel }}</td>
+
+    </tr>
+
+      @endforeach
+
+    </table>
+    {{ $students ->links() }}
+
+
+@endsection
