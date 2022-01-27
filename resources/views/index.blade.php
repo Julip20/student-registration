42e202eeaba5a42e46b87e622f9deb87bed531cd
criss julip <christopherkennedy459@gmail.com>
2022-01-27 09:39:17 +0300

    fixed the index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="pull-left">
-                <h2>Student Registration Form</h2>
+                <h2>List of registered Students</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('students.create') }}"> Register Here</a>
