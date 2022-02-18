58289d81f3dfd4b30ccd545a4d6404bc28bce799
criss julip <christopherkennedy459@gmail.com>
2022-02-18 21:10:47 +0300

    Trying to add spatie permissions

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -16,50 +16,53 @@
 @endif
 <div class="row">
     <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
 
-        </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('students.index') }}"> Back</a>
         </div>
     </div>
 </div>
 
-
-
-<form class="needs-validation" action="{{ route('students.store') }}" method="POST" novalidate>
-    @csrf
+<div class="container">
     <div class="col d-flex justify-content-center">
+
     <div class="card" style="width:800px" >
         <h2 class="card-header">STUDENT REGISTRATION</h2>
-     <div class="container">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Name">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>O-Level:</strong>
-                <input type="text" name="olevel" class="form-control" placeholder="O-Level">
-            </div>
-        </div>
+        <form class="needs-validation" action="{{ route('students.store') }}" method="POST" novalidate>
+        @csrf
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>A-Level:</strong>
-                <input type="text" name="alevel" class="form-control" placeholder="A-Level">
+
+
+
+
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>O-Level:</strong>
+                    <input type="text" name="olevel" class="form-control" placeholder="O-Level">
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                 <div class="text-right">
-                <button type="submit" class="btn btn-primary">Submit</button>
-                 </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>A-Level:</strong>
+                    <input type="text" name="alevel" class="form-control" placeholder="A-Level">
+                </div>
             </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                     <div class="text-right">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                     </div>
+                </div>
+
+
+
+    </form>
     </div>
-</div>
-</div>
-</form>
-
+    </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,3 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::resource('students',StudentsController::class);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
